b82aece378d560ee1fb8a70aab35faab80d3d104
ig-oramos <56934942+ig-oramos@users.noreply.github.com>
2020-03-10 20:06:50 -0300

    Adição da implementação dos métodos
    Implementações dos métodos: autoCPF, autoTelefone e autoCelular. Todos métodos de auto preenchimento/formatação de campos.

--- Cidadao.js
+++ Cidadao.js
@@ -1,4 +1,3 @@
-/* Lista de caracteres para validação */
 class Cidadao {
     constructor(nome, rg, cpf, telefone, celular, email, senha) {
         this.nome = nome
@@ -17,32 +16,75 @@ class Cidadao {
         let rgLength = rg_array.length
         if (rgLength >= 2 && rgLength <= 4)
             rg_array.splice(2, 0, '.')
-        else if (rgLength >= 5 && rgLength <= 7) {
+        else if (rgLength <= 7) {
             rg_array.splice(2, 0, '.')
             rg_array.splice(6, 0, '.')
-        } else if (rgLength >= 8 && rgLength <= 9) {
+        } else if (rgLength <= 9) {
             rg_array.splice(2, 0, '.')
             rg_array.splice(6, 0, '.')
             rg_array.splice(10, 0, '-')
         }
-        else if (this.getRg().length == 11)
-            rg.array.push('-')
-        else {
 
-        }
-        return rg_array.toString()
+        return rg_array.toString().replace(/\,/g, '')
     }
 
     autoCpf() {
-
+        let cpf_clean = this.getCpf().replace(/[\.-]/g, '')
+        const cpf_array = cpf_clean.split('') // Quebra rg num array
+        
+        let cpfLength = cpf_array.length
+        if (cpfLength >= 3 && cpfLength <= 5)
+            cpf_array.splice(3, 0, '.')
+        else if (cpfLength <= 8) {
+            cpf_array.splice(3, 0, '.')
+            cpf_array.splice(7, 0, '.')
+        } else if (cpfLength <= 11) {
+            cpf_array.splice(3, 0, '.')
+            cpf_array.splice(7, 0, '.')
+            cpf_array.splice(11, 0, '-')
+        }
+    
+        return cpf_array.toString().replace(/\,/g, '')        
     }
 
     autoTelefone() {
-
+        let tell_clean = this.getTelefone().replace(/[-\(\)]/g, '')
+        const tell_array = tell_clean.split('') // Quebra rg num array
+        
+        let tellLength = tell_array.length
+        if (tellLength >= 0 && tellLength <= 1)
+            tell_array.splice(0, 0, '(')
+        else if (tellLength <= 5) {
+            tell_array.splice(0, 0, '(')
+            tell_array.splice(3, 0, ')')
+        } else if (tellLength <= 10) {
+            tell_array.splice(0, 0, '(')
+            tell_array.splice(3, 0, ')')
+            tell_array.splice(8, 0, '-')
+            
+        }
+    
+        return tell_array.toString().replace(/\,/g, '')
     }
 
     autoCelular() {
-
+        let cell_clean = this.getCelular().replace(/[-\(\)]/g, '')
+        const cell_array = cell_clean.split('') // Quebra rg num array
+        
+        let cellLength = cell_array.length
+        if (cellLength >= 0 && cellLength <= 1)
+            cell_array.splice(0, 0, '(')
+        else if (cellLength <= 6) {
+            cell_array.splice(0, 0, '(')
+            cell_array.splice(3, 0, ')')
+        } else if (cellLength <= 11) {
+            cell_array.splice(0, 0, '(')
+            cell_array.splice(3, 0, ')')
+            cell_array.splice(9, 0, '-')
+            
+        }
+    
+        return cell_array.toString().replace(/\,/g, '')
     }
 
     // Getters
